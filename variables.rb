0fd6de2a33e01f90293e23efbc61a51f7e16670d
Scott Sullivan <scottsullivanltd@gmail.com>
2022-01-10 20:33:44 -0700

    Add integers

--- variables.rb
+++ variables.rb
@@ -1,3 +1,7 @@
 favorite__nfl_team = "Denver Broncos"
 favorite_mlb_team = "Colorado Rockies"
 favorite_nhl_team = "Colorado Avelanche"
+
+number_of_pets = 2
+number_of_kids = 1
+number_of_cousins = 11
